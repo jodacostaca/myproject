ed142fca159d08627834dc16c1f5a9a47b457765
Josué Acosta <jodacostaca@gmail.com>
2025-03-05 20:06:58 -0500

    Ejercicios Javascript

---
+++ Javascript/index/app.js
@@ -0,0 +1,29 @@
+console.log("Hola " + " mundo"); //Concatenación de scripts
+console.log("hola mundo"); //meter a la consola 
+let a=6;
+let b= 3;
+console.log(a+b)
+console.log(a*b)
+console.log(a/b)
+console.log(a-b)
+console.log(a)
+a += 10
+console.log(a)
+a -= 2
+console.log(a)
+a *= 2
+console.log(a)
+a /= 2
+console.log(a)
+let edad = 25
+let tienelicencia = true
+ if(edad>= 14 && tienelicencia){
+    console.log("Puede consucir un automovil")
+ }
+
+edad -= 25;
+ if(edad>= 14 && tienelicencia){
+    console.log("Puede consucir un automovil")
+ } else{
+    console.log("no puede manejar")
+ }
